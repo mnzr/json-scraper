2f1ac436efc6a1affa86fad61cd2133b69d0de60
Ratul Minhaz <minhaz.ratul@gmail.com>
2015-07-08 00:09:08 +0600

    Added func for App access

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 fb-scrap
 ========
+
+Simple Python script to scrape info from any Facebook page.
+Libraries used:
+- Requests
--- __init__.py
+++ __init__.py
@@ -1,61 +1,61 @@
 #!/usr/bin/env python
-import urllib2
-import json
-import mysql.connector
+"""Script to get Facebook info using Graph API"""
+import requests
 
-def connect_db():
-    #db connection info
-    connection = mysql.connector.connect(user='rr', password='rr',
-                                         database='facebook_data')
-    return  connection
+
+def create_post_url(graph_url, app_id, app_secret):
+    """to make the URL"""
+    post_args = "/?key=value&access_token=" + app_id + "|" + app_secret
+    post_url = graph_url + post_args
+
+    return post_url
+
+
+# def connect_db():
+#     """connecting to DB"""
+#     connection = mysql.connector.connect(user='rr', password='rr',
+#                                          database='facebook_data')
+#     return connection
 
 
 def main():
-    #simply facebook usernames
+    """The main function"""
     list_companies = ["walmart", "cisco", "pepsi", "facebook"]
-    graph_url = "http://graph.facebook.com/"
+    graph_url = "https://graph.facebook.com/"
+    app_id = "342918142392056"
+    app_secret = "b52b21c24c870c5c139cca42f03ada7b"
 
-    #create db connection
-    connection = connect_db()
-    cursor = connection.cursor()
-
-    #MySQL statement for adding Facebook db
-    insert_info = ("INSERT INTO page_info "
-                    "(fb_id, likes, talking_about, username)"
-                    "VALUES (%s, %s, %s, %s)")
+    # Show company list
+    print "Companies in our list:"
+    for company in list_companies:
+        print "> ", company
+    print
 
     for company in list_companies:
-        #make graph api url with company username
-        current_page = graph_url + company
+        # make graph api url with company username
+        current_page = graph_url + \
+                       create_post_url(company, app_id, app_secret)
 
-        #open public page in facebook graph api
-        web_response = urllib2.urlopen(current_page)    #take response from the url
-        readable_page = web_response.read()             #conversion to human readable format
-        json_fbpage = json.loads(readable_page)
+        # open public page in facebook graph api
+        # take response from the url
+        web_response = requests.get(current_page)
+        # conversion to human readable format
+        json_fbpage = web_response.json()
 
-        #print page data to console
+        # print page data to console
         print company, "page"
-        print json_fbpage["id"]
-        print json_fbpage["likes"]
-        print json_fbpage["talking_about_count"]
-        print json_fbpage["link"]
-        print json_fbpage["username"]
-        print json_fbpage["website"]
+        print "Page ID: ", json_fbpage["id"]
+        print "Total likes: ", json_fbpage["likes"]
+        print "People talking about it: ", json_fbpage["talking_about_count"]
+        print "Link to Facebook Page: ", json_fbpage["link"]
+        print "Facebook username: ", json_fbpage["username"]
+        print "Website: ", json_fbpage["website"]
         print ""
 
-
-        #gather JSON Data
-        page_data = (json_fbpage["id"], json_fbpage["likes"],
-                     json_fbpage["talking_about_count"],
-                     json_fbpage["username"])
-
-        #insert the data we pulled into db
-        cursor.execute(insert_info, page_data)
-
-        #commit the data to the db
-        connection.commit()
-
-    connection.close()
+        # gather JSON Data
+        # page_data = (json_fbpage["id"], json_fbpage["likes"],
+        #              json_fbpage["talking_about_count"],
+        #              json_fbpage["username"])
 
 if __name__ == '__main__':
     main()
