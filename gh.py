ea37bb2e0661526faa35923dccf1bcffdbaf3fb5
mnzr <minhaz.ratul@gmail.com>
2015-07-13 10:25:43 +0000

    To show Rakib a demo

---
+++ gh.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""Script to get Github info using API"""
+import requests
+
+
+def main():
+    # Add any number of usernames here
+    users = ['google', 'facebook', 'apache']
+    url = "https://api.github.com/users/"
+    # Which JSON properties we want to catch
+    properties = ['login', 'id', 'html_url', 'public_repos', 'created_at']
+
+    for user in users:
+        # make API url with Github username
+        current_page = url + user
+        # take response from the url
+        response = requests.get(current_page)
+        # conversion to human readable format
+        json = response.json()
+        for prop in properties:
+            print prop, ':', json[prop]
+        print ''
+
+
+if __name__ == '__main__':
+    main()
