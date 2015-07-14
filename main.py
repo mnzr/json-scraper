2278fe21c96d8d51022e7b5dda4e2e34baae248d
Ratul Minhaz <minhaz.ratul@gmail.com>
2015-07-14 11:39:21 +0600

    modigied facebook scraping

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 """Script to get Facebook info using Graph API"""
 import requests
+from sys import argv
+import rethinkdb as r
+from rethinkdb.errors import RqlRuntimeError
+
+SCRAP_DB = "fbscrap"
 
 
 def create_post_url(graph_url, app_id, app_secret):
@@ -11,19 +16,60 @@ def create_post_url(graph_url, app_id, app_secret):
     return post_url
 
 
-# def connect_db():
-#     """connecting to DB"""
-#     connection = mysql.connector.connect(user='rr', password='rr',
-#                                          database='facebook_data')
-#     return connection
+def connect_db():
+    """connecting to rethinkDB"""
+    r.connect('localhost', 28015).repl()
+    connection = r.connect(host='localhost',
+                           port=28015,
+                           db='fbscrap')
+    return connection
+
+
+# Setting up the app database
+# Creating a table `fbscrap` in the database named in `TODO_DB` variable.
+# We'll create the database and table here using
+# [`db_create`](http://www.rethinkdb.com/api/python/db_create/)
+# and
+# [`table_create`](http://www.rethinkdb.com/api/python/table_create/) commands.
+def setup_db():
+    """to setup a DB"""
+    connection = r.connect(host='localhost',
+                           port=28015)
+    try:
+        r.db_create(SCRAP_DB).run(connection)
+        r.db(SCRAP_DB).table_create('fbscrap').run(connection)
+        print 'Using newly created database.'
+    except RqlRuntimeError:
+        print 'Using existing database.'
+    finally:
+        connection.close()
+
+
+def insert_db(name, id, likes, talking_about_count, link, username,
+              website):
+    """ print page data to console"""
+    # connection = r.connect(host='localhost',
+                           # port=28015,
+                           # db='fbscrap')
+    r.table('fbscrap').insert(
+        {
+            'name': name,
+            'id': id,
+            'likes': likes,
+            'talking_about_count': talking_about_count,
+            'link': link,
+            'username': username,
+            'website': website
+        }).run(connect_db())
 
 
 def main():
     """The main function"""
-    list_companies = ["walmart", "cisco", "pepsi", "facebook"]
+    # list_companies = ["walmart", "cisco", "pepsi", "facebook"]
+    list_companies = ["walmart"]
     graph_url = "https://graph.facebook.com/"
-    app_id = "342918142392056"
-    app_secret = "b52b21c24c870c5c139cca42f03ada7b"
+    app_id = argv[1]
+    app_secret = argv[2]
 
     # Show company list
     print "Companies in our list:"
@@ -42,20 +88,24 @@ def main():
         # conversion to human readable format
         json_fbpage = web_response.json()
 
-        # print page data to console
-        print company, "page"
-        print "Page ID: ", json_fbpage["id"]
-        print "Total likes: ", json_fbpage["likes"]
-        print "People talking about it: ", json_fbpage["talking_about_count"]
-        print "Link to Facebook Page: ", json_fbpage["link"]
-        print "Facebook username: ", json_fbpage["username"]
-        print "Website: ", json_fbpage["website"]
-        print ""
+        # insert JSON Data to rethinkDB
 
-        # gather JSON Data
-        # page_data = (json_fbpage["id"], json_fbpage["likes"],
-        #              json_fbpage["talking_about_count"],
-        #              json_fbpage["username"])
+        insert_db(company,
+                  json_fbpage["id"],
+                  json_fbpage["likes"],
+                  json_fbpage["talking_about_count"],
+                  json_fbpage["link"],
+                  json_fbpage["username"],
+                  json_fbpage["website"])
+
+        print "So the big bad DB has this info now:"
+        for doc in r.db("fbscrap").table("fbscrap").run(connect_db()):
+            for key in doc:
+                print key, " ", doc[key]
+            print ""
+
+
+r.db("fbscrap").table("fbscrap")
 
 if __name__ == '__main__':
     main()
