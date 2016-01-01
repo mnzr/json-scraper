90c4ce1a74680a715e70d1095136413c38776319
Ratul Minhaz <minhaz.ratul@gmail.com>
2016-01-01 19:06:12 +0600

    removed main()

--- gh.py
+++ gh.py
@@ -3,24 +3,19 @@
 import requests
 
 
-def main():
-    # Add any number of usernames here
-    users = ['google', 'facebook', 'apache']
-    url = "https://api.github.com/users/"
-    # Which JSON properties we want to catch
-    properties = ['login', 'id', 'html_url', 'public_repos', 'created_at']
+# Add any number of usernames here
+users = ['google', 'facebook', 'apache']
+url = "https://api.github.com/users/"
+# Which JSON properties we want to catch
+properties = ['login', 'id', 'html_url', 'public_repos', 'created_at']
 
-    for user in users:
-        # make API url with Github username
-        current_page = url + user
-        # take response from the url
-        response = requests.get(current_page)
-        # conversion to human readable format
-        json = response.json()
-        for prop in properties:
-            print prop, ':', json[prop]
-        print ''
-
-
-if __name__ == '__main__':
-    main()
+for user in users:
+    # make API url with Github username
+    current_page = url + user
+    # take response from the url
+    response = requests.get(current_page)
+    # conversion to human readable format
+    json = response.json()
+    for prop in properties:
+        print(prop, ':', json[prop])
+    print('')
